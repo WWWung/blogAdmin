92375f73095d6119ea2919a05eb14a54cdcba1b7
wwwung <wwwungj@163.com>
2018-07-18 17:55:44 +0800

    修改

--- src/views/blog.vue
+++ src/views/blog.vue
@@ -3,10 +3,15 @@
 		<el-main>
 			<h2>博客列表</h2>
 			<el-table 
-				stripe
-				:data="data" 
+				:data="blogs.data" 
 				style="width: 1400px;"
-				v-loading="blogLoading">
+				v-loading="blogLoading"
+				@current-change='checkBloglistRow'
+				highlight-current-row
+				ref='blogtable'>
+				<el-table-column 
+					type='index'
+					fixed></el-table-column>
 				<el-table-column 
 					prop="id" 
 					label='博客ID' 
@@ -53,7 +58,7 @@
 					<template slot-scope="scope">
 						<el-date-picker 
 							type="date" 
-							v-model="scope.row.date"
+							v-model="scope.row.time"
 							placeholder="选择日期"
 							align="center"></el-date-picker>
 					</template>
@@ -63,7 +68,7 @@
 					width="180"
 					fixed="right">
 					<template slot-scope="scope">
-						<el-button size="mini">
+						<el-button size="mini" @click="changeBlogInfo(scope.row)">
 							保存
 						</el-button>
 						<el-button type="danger" size="mini">
@@ -75,26 +80,46 @@
 			<el-pagination 
 					align="center"
 					backround
-					:page-size="1"
+					:page-size="blogs.pageCount"
 					layout="prev, pager, next"
-					:total="10"></el-pagination>
+					:total="blogs.total"
+					:current-page="blogs.page"
+					@current-change='blogTablePageChange'></el-pagination>
 			<h2>博客评论</h2>
 			<el-table 
 				stripe
-				:data="commentData">
+				:data="comments.data"
+				v-loading="commentLoading">
 				<el-table-column prop="id" label='评论ID' width="100"></el-table-column>
-				<el-table-column prop="content" label='内容' width="180"></el-table-column>
-				<el-table-column prop="user" label='作者' width="180"></el-table-column>
-				<el-table-column prop="date" label='日期' width="240">
+				<el-table-column prop="content" label='内容' width="380"></el-table-column>
+				<el-table-column prop="name" label='作者' width="180"></el-table-column>
+				<el-table-column prop="time" label='日期' width="240">
 					<template slot-scope="scope">
 						<el-date-picker 
 							type="date" 
-							v-model="scope.row.date"
+							v-model="scope.row.time"
 							placeholder="选择日期"
 							align="center"></el-date-picker>
 					</template>
 				</el-table-column>
+				<el-table-column 
+					label='操作' 
+					width="180">
+					<template slot-scope="scope">
+						<el-button type="danger" size="mini">
+							删除
+						</el-button>
+					</template>
+				</el-table-column>
 			</el-table>
+			<el-pagination 
+					align="center"
+					backround
+					:page-size="comments.pageCount"
+					layout="prev, pager, next"
+					:total="comments.total"
+					:current-page="comments.page"
+					@current-change='commentTablePageChange'></el-pagination>
 		</el-main>
 	</div>
 </template>
@@ -103,9 +128,21 @@
 	export default {
 		data () {
 			return {
-				data: [],
-				commentData: [],
-				blogLoading: false
+				blogLoading: false,
+				commentLoading: false,
+				blogs: {
+					data: [],
+					total: 0,
+					page: 1,
+					pageCount: 10
+				},
+				comments: {
+					data: [],
+					total: 0,
+					page: 1,
+					pageCount: 10
+				},
+				currentBlogId: null
 			}
 		},
 		methods: {
@@ -116,26 +153,56 @@
 			add0 (num) {
 				return num < 10 ? '0' + num : num + ''
 			},
-			getData () {
+			getBlogData () {
 				this.blogLoading = true
-				const url = '/bloglist'
+				const url = '/bloglist?page=' + this.blogs.page + '&pageCount=' + this.blogs.pageCount
 				this.ajax.get(url).then(d => {
-					console.log(d)
 					this.blogLoading = false
-					this.data = d.data.data
+					this.blogs = d.data
+				}).catch(err => {
+					this.blogLoading = false
+				})
+			},
+			blogTablePageChange (val) {
+				this.blogs.page = val
+				this.getBlogData()
+			},
+			checkBloglistRow (row) {
+				this.$refs.blogtable.setCurrentRow(row);
+				if (row) {
+					this.changeCurrentBlogId(row);
+				}
+			},
+			changeCurrentBlogId ({id}) {
+				this.currentBlogId = id
+				this.getBlogComment()
+			},
+			getBlogComment () {
+				this.commentLoading = true
+				const url = '/comments?page=' + this.comments.page + '&pageCount=' + this.comments.pageCount + '&id=' + this.currentBlogId
+				this.ajax.get(url).then(d => {
+					this.comments = d.data
+					this.commentLoading = false
 				}).catch(err => {
 					console.log(err)
-					this.blogLoading = false
+					this.commentLoading = false
+				})
+			},
+			commentTablePageChange (val) {
+				this.comments.page = val
+				this.getBlogComment()
+			},
+			changeBlogInfo (blog) {
+				const url = '/editblog?id=' + blog.id
+				this.ajax.post(url, JSON.stringify(blog)).then(d => {
+					console.log(d)
+				}).catch(err => {
+					console.log(err)
 				})
 			}
 		},
 		created () {
-//			this.getData()
-			this.ajax.get('/test').then(d => {
-				console.log(d)
-			}).catch(err => {
-				console.log(err)
-			})
+			this.getBlogData()
 		}
 	}
 </script>
